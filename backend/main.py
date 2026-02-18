92c50718d4689063085d8605da1ef7824ff327fe
EnbekAI Dev <dev@enbek.ai>
2026-02-18 17:59:35 +0500

    fix: cors for vercel

--- backend/main.py
+++ backend/main.py
@@ -30,8 +30,8 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
